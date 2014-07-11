5c7a516b06f4aa3c083a89db02f13e40f1718648
Morf-A <Morf-a@yandex.ru>
2014-07-11 13:15:16 +0300

    Добавил сохранение нового поста

--- lib/PerlBlog/Controller/Post.pm
+++ lib/PerlBlog/Controller/Post.pm
@@ -22,17 +22,40 @@ sub form_create {
 sub create {
     my $self = shift;
     
-    #Получаем параметры
-    
+    #Сохраняем пост
+
     my $title     = $self->param('title');
     my $content   = $self->param('content');
     my $preview   = $self->param('preview');
-    my @tagIds       = $self->param('tagsToController');
+    
+    my $login = $self->session('login');
+    my $author_id = PerlBlog::Model::User->select({login => $login})->hash()->{id};
+    
+    
+    my %post = (
+        title     => $title,
+        content   => $content,
+        preview   => $preview,
+        author_id => $author_id
+    );
+    
+    # Проверка
+    unless($title) {
+        $self->flash(error => 'Title can not be empty')->redirect_to('post_form_create');
+        return;
+    }
+    my $post_id = PerlBlog::Model::Post->insert(\%post);
+   
+    
+    # Теперь привязываем к посту теги
+    my @tagIds = $self->param('tagsToController');
 
-    
-    my $author = $self->session('login');
-    
-    #  my $user_id = PerlBlog::Model::User->insert(\%post);
+    foreach my $tagId (@tagIds){
+        PerlBlog::Model::PostsTags->insert({
+                tag_id  => $tagId,
+                post_id => $post_id
+        });
+    }
     
     $self->redirect_to('/');
 }
---
+++ lib/PerlBlog/Model/PostsTags.pm
@@ -0,0 +1,10 @@
+package PerlBlog::Model::PostsTags;
+use strict;
+use warnings;
+use v5.10;
+use base qw/PerlBlog::Model::Base/;
+
+sub table_name {'posts_tags'}
+
+1;
+
