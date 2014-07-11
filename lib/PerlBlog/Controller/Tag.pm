7743ef05680fd00e0dc9cbf91d74177ce34b8ea0
Morf-A <Morf-a@yandex.ru>
2014-07-11 19:43:50 +0300

    Выделил общий лайаут для лент

---
+++ lib/PerlBlog/Controller/Tag.pm
@@ -0,0 +1,25 @@
+package PerlBlog::Controller::Tag;
+
+use strict;
+use warnings;
+use v5.10;
+use base 'Mojolicious::Controller';
+use Data::Dumper;
+
+sub main {
+    my $self = shift;
+    # Пользователь
+    my $userName = $self->session('login');
+
+    # Посты
+    my $allPosts = PerlBlog::Model::Post->get_posts();
+  
+    
+    print Dumper($allPosts);
+    
+    $self->render(userName=>$userName, posts=>$allPosts);  
+}
+
+
+1;
+
